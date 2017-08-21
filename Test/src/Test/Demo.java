97bee16aff2cac4ed74d09b375b2bed29887dc50
Administrator <Administrator@A-201705270844>
2017-08-21 21:12:09 +0800

    1

--- Test/src/Test/Demo.java
+++ Test/src/Test/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("11");
-		System.out.println("212");
+		System.out.println("2112");
 	}
 
 }
